1c68f49383cd18ac26f32072eb01427649209838
Geetanjali Makineni <59195740+geetamakineni@users.noreply.github.com>
2020-06-21 21:43:05 -0500

    updating ICP3

--- ICP3/Source Code/RWD/script1.js
+++
@@ -1,9 +0,0 @@
-
-$( document ).ready(function() {
-    // $("#s1").click(function () {
-    //     $(".text").css("color", "#00FF15");
-    // });
-    // $("#s2").click(function () {
-    //     $(".text").css("color", "#FC0000");
-    // });
-});
