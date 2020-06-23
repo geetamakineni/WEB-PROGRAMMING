ae0ee7883a711daac4e6e6331d8fa59973693988
Geetanjali Makineni <59195740+geetamakineni@users.noreply.github.com>
2020-06-22 21:08:26 -0500

    Add files via upload

--- ICP4/Source code/GitHubUserFinder/script.js
+++ ICP4/Source code/GitHubUserFinder/script.js
@@ -1,74 +1,67 @@
+/*
+* This method will make a get call to the Github API and the call appropriate methods.
+* If user found the we are calling showUser method and passing the response.
+* else calling noSuchUser method.
+*/
 function getGithubInfo(user) {
-    //1. Create an instance of XMLHttpRequest class and send a GET request using it.
-    // The function should finally return the object(it now contains the response!)
-        
-        var username = user;
-        var requri   = 'https://api.github.com/users/'+username;
-        var repouri  = 'https://api.github.com/users/'+username+'/repos';
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if ( xhr.readyState === 4) {
-                jsonResponse = JSON.parse(xhr.responseText);
-                showUser(jsonResponse,repouri);
-                console.log(xhr.status);
-                console.log(xhr.readyState);
+    //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(resp) {
+        //if the response is successful show the user's details
+        if(this.readyState == 4 ) {
+            if (this.status == 200) {
+                showUser(JSON.parse(this.responseText));
+                //else display suitable message
+            } else {
+                noSuchUser(username);
             }
-
         }
-        xhr.open('GET', requri, true);
-        xhr.send('');
+    };
+    xhttp.open("GET", "https://api.github.com/users/"+user, true);
+    xhttp.send();
+    return xhttp.responseText;
 }
 
-function showUser(user,repouri) {
-    // debugger;
-    var fullname   = user.name;
-        var username   = user.login;
-        var aviurl     = user.avatar_url;
-        var profileurl = user.html_url;
-        var location   = user.location;
-        var followersnum = user.followers;
-        var followingnum = user.following;
-        var reposnum     = user.public_repos;
-        
-        if(fullname == undefined) { fullname = username; }
-        
-        var outhtml = '<h2>UserName:'+fullname+' <span class="smallname"><br>Profile link:<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
-        outhtml = outhtml + '<div class="ghcontent"><br><div class="avi"><br><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
-        outhtml = outhtml + '<br>Repos: '+reposnum+'</p></div>';
-
-
-        var repositories;
-        $.getJSON(repouri, function(json){
-          repositories = json;   
-          outputPageContent();                
-        }); 
-        function outputPageContent() {
-            if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
-            else {
-              outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
-              $.each(repositories, function(index) {
-                outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
-              });
-              outhtml = outhtml + '</ul></div>'; 
-            }
-        
-        $('#profile').html(outhtml);
-        }
+/*
+* This method will display the Name, image , Github profile and repository links
+*/
+function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    $("#profile").find("h2").text(user.name);
+    $("#profile").find("h3").text(user.login);
+    $(".avatar").prepend('<img src="'+user.avatar_url+'" width="100px" height="100px"/>')
+    $(".information").prepend('<a href="'+user.html_url+'">Github Url</a></br><a href="'+user.repos_url+'">Repo Url</a>');
 }
 
+/*
+* This method will call when user not found. It will display message No such profile username.
+*/
 function noSuchUser(username) {
-    alert('No Username Found');
-    //3. set the elements such that a suitable message is displayed
+    $("#profile").find("h2").text("No such profile" + username);
 }
 
-$(document).ready(function () {
-    $(document).on('keypress', '#username', function (e) {
+/*
+* This method will call when the document is ready.
+*/
+$(document).ready(function(){
+    /*
+    * This method will call when user type in the input.
+    * First we are clearing the display content then if user press enter, it will go to if statement.
+    * In If statement we are taking value from input and then storing in username then we are passing username to getGithubInfo(username) method.
+    */
+    $(document).on('keypress', '#username', function(e){
         //check if the enter(i.e return) key is pressed
+        $("#profile").find("h2").text("");
+        $(".avatar").html('')
+        $(".information").html('');
         if (e.which == 13) {
+            //get what the user enters
             username = $(this).val();
+            //reset the text typed in the input
             $(this).val("");
+            //get the user's information and store the respsonse
             getGithubInfo(username);
         }
     })
 });
+
